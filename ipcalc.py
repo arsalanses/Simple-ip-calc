338ca0e79e003b8ebb0bcc37f0405fe0d975fcda
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-11-25 15:30:12 +0330

    Fix verify function equal 255

--- ipcalc.py
+++ ipcalc.py
@@ -2,7 +2,7 @@ import sys
 
 def verify(ip):
     for item in ip.split('.'):
-        if int(item) >= 255:
+        if int(item) > 255:
             print("Invalid ip address")
             return False
     
