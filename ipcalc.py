009211b20eeff199b433887d9499ca32e69cdaba
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-11-25 13:54:48 +0330

    Add print network ip function

--- ipcalc.py
+++ ipcalc.py
@@ -5,7 +5,21 @@ def verify(ip):
         if int(item) > 255:
             return False
 
-ip = input("Address: ")
+def to_bin(ip):
+    element = list(map(int, ip.split('.')))
+    
+    bin_num = []
+    for item in element:
+        bin_num.append(int(bin(item)[2:]))
+    
+    return "{:08d}.{:08d}.{:08d}.{:08d}".format(bin_num[0], bin_num[1], bin_num[2], bin_num[3])
+
+def print_address(ip):
+    print("Network: {} \t {}".format(ip, to_bin(ip)))
+
+ip = input("Enter ip address: ")
 
 if not verify(ip):
     sys.exit(0)
+
+print_address(ip)
